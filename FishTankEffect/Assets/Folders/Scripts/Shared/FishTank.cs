e5cfd80ec01d5d9467a6da786ced8385d3460c2c
LowLevelLemmy <77054159+LowLevelLemmy@users.noreply.github.com>
2021-08-13 09:21:12 -0700

    Added lerping wah wah

--- FishTankEffect/Assets/Folders/Scripts/Shared/FishTank.cs
+++ FishTankEffect/Assets/Folders/Scripts/Shared/FishTank.cs
@@ -6,6 +6,8 @@ using DG.Tweening;
 public class FishTank : MonoBehaviour, IShootable
 {
     Material mat;
+    float gunshotPoint = 999999999;
+    float timeItTakesForTheEntireTankToEmpty = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -15,9 +17,32 @@ public class FishTank : MonoBehaviour, IShootable
 
     public void OnShot(RaycastHit hit)
     {
-        print("SHOTTT!");
+        // TODO: Generate gunshot decal
+        // TODO: spawn water leaking particle effect for the duration of the thingy
+
+        float factor = CalcGunshotFactor(hit);
+        if (factor < gunshotPoint)
+        {
+            gunshotPoint = factor;
+            LerpToGunshotPoint();
+        }
+    }
+
+    float CalcGunshotFactor(RaycastHit hit)
+    {
         float abba = hit.collider.transform.InverseTransformPoint(hit.point).y;
-        float factor = abba.Remap(-0.5f, 0.5f, 0, 1);
-        mat.SetFloat("GradientMultiplier", factor);
+        return abba.Remap(-0.5f, 0.5f, 0, 1);
+    }
+
+    void LerpToGunshotPoint()
+    {
+        mat.DOComplete();   // Clear previous tweens... hopefully?
+
+        float previousValue = mat.GetFloat("GradientMultiplier");
+        float distance = previousValue - gunshotPoint;
+
+        float duration = distance * timeItTakesForTheEntireTankToEmpty;
+
+        mat.DOFloat(gunshotPoint, "GradientMultiplier", duration);
     }
 }
